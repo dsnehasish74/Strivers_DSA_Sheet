9f38113bb37ea1140c1562597ac2437b7224276c
Snehasish Dhar <dsnehasish74@gmail.com>
2022-06-10 23:56:45 +0530

    Added Day 5

---
+++ Day5/FindDuplicate.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+int findDuplicate(vector<int> &nums, int n)
+{
+    // Write your code here.
+    int slow = nums[0], fast = nums[0];
+    do
+    {
+        slow = nums[slow];
+        fast = nums[nums[fast]];
+    } while (slow != fast);
+
+    fast = nums[0];
+
+    while (slow != fast)
+    {
+        slow = nums[slow];
+        fast = nums[fast];
+    }
+
+    return slow;
+}
