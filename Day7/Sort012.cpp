d7fd5aa1f368b2f8f903c05b539e7b5520319d54
Snehasish Dhar <dsnehasish74@gmail.com>
2022-06-12 23:37:37 +0530

    Day7

---
+++ Day7/Sort012.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+void sort012(int *nums, int n)
+{
+    //   Write your code here
+    int low = 0, mid = 0, high = n - 1;
+    while (mid <= high)
+    {
+        if (nums[mid] == 0)
+        {
+            swap(nums[mid], nums[low]);
+            low++;
+            mid++;
+        }
+        else if (nums[mid] == 2)
+        {
+            swap(nums[mid], nums[high]);
+            high--;
+        }
+        else
+        {
+            mid++;
+        }
+    }
+}
